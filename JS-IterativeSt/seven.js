d6e435778f4b5886759af98eefdad9b5b162746a
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-12-20 11:35:51 +0530

    okie

---
+++ JS-IterativeSt/seven.js
@@ -0,0 +1,14 @@
+//0,2,5,9,14, 20,  27, 35
+//   +2 +3 +4+5+6 +7  +8
+
+/*
+  initlization let i=0
+  condition :  i<=35
+  j = 2
+  incr :       i=i+j;j++
+*/
+var j = 2
+for (let i = 0; i <= 35; j++) {
+    console.log(i)
+    i = i + j
+}
